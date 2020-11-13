1902436d77c11c5eac584d28e7c25775146321c7
saurav ghimire <48352478+sauravvgh@users.noreply.github.com>
2020-11-13 10:48:21 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write(num1 + num2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
